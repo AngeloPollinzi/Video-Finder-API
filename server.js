168837bca29a2ecc57587f91c7425dc3d0adea72
Angelo <Angelo@Zagarisaaa>
2018-12-09 20:13:54 +0100

    last fixes

--- server.js
+++ server.js
@@ -3,25 +3,24 @@ var fs = require('fs');
 var path = require('path');
 
 http.createServer(function(req, res){
-
     if(req.url === "/"){
-        fs.readFile("../WebContent/index.html", "UTF-8", function(err, html){
+        fs.readFile("WebContent/index.html", "UTF-8", function(err, html){
             res.writeHead(200, {"Content-Type": "text/html"});
             res.end(html);
         });
     }else if(req.url.match("\.css$")){
-        var cssPath = path.join('../WebContent', req.url);
+        var cssPath = path.join('WebContent', req.url);
         var fileStream = fs.createReadStream(cssPath, "UTF-8");
         res.writeHead(200, {"Content-Type": "text/css"});
         fileStream.pipe(res);
 
     }else if(req.url.match("\.png$")){
-        var imagePath = path.join('../WebContent', req.url);
+        var imagePath = path.join('WebContent', req.url);
         var fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, {"Content-Type": "image/png"});
         fileStream.pipe(res);
     }else if(req.url.match("\.jpg$")){
-        var imagePath = path.join('../WebContent', req.url);
+        var imagePath = path.join('WebContent', req.url);
         var fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, {"Content-Type": "image/jpg"});
         fileStream.pipe(res);
